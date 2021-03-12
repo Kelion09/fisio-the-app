63c81fcf16be729b5b7a464bcb080b6c258965ba
Larissa Nahan Dantas <larissanahan@hotmail.com>
2021-03-12 12:53:44 -0300

    Page Atividade

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Atividade from '../views/Atividade.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/atividade',
+    name: 'Atividade',
+    component: Atividade
+  }  
+]
 
 const router = new VueRouter({
   routes
